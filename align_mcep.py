52cb0cb61ed153feb4fb08fda45bf08256a2046b
23CH3COOH <kurimannjuu2_ybb_ne_jp@yahoo.co.jp>
2022-05-08 21:54:08 +0900

    main側でフォルダやファイルパスを整えるようにリファクタリング

--- align_mcep.py
+++ align_mcep.py
@@ -1,40 +1,23 @@
 # -*- coding: utf-8 -*-
-import os
 import numpy as np
-from glob import glob
 from scipy.spatial.distance import euclidean
 from dtw import dtw
 
 
-def align_mcep(conv_from, conv_to):
-    align_from = 'mcep_aligned/%s_to_%s/%s/' % (conv_from, conv_to, conv_from)
-    align_to = 'mcep_aligned/%s_to_%s/%s/' % (conv_from, conv_to, conv_to)
-    if not os.path.exists(align_from):
-        os.makedirs(align_from)
-    if not os.path.exists(align_to):
-        os.makedirs(align_to)
-    mcep_dir_from = 'mcep/%s/' % conv_from
-    mcep_dir_to = 'mcep/%s/' % conv_to
+'''
+2つのmcepファイルin_path_1とin_path_2アライメント（時間同期）をとり、
+それぞれout_path_1とout_path_2に出力する
+'''
+def align_mcep(in_path_1, in_path_2, out_path_1, out_path_2):
+    # オリジナルのmcepを読み込み
+    mcep1 = np.loadtxt(in_path_1)
+    mcep2 = np.loadtxt(in_path_2)
 
-    for mcep_path1 in glob(mcep_dir_from + '*.mcep'):
-        file_name = os.path.basename(mcep_path1)
-        mcep_path2 = mcep_dir_to + file_name
-        print(file_name)
+    # DTWで同期を取る（dtwには引数distが必要）
+    dist, cost, dummy, path = dtw(mcep1, mcep2, dist=euclidean)
+    aligned_mcep1 = mcep1[path[0]]
+    aligned_mcep2 = mcep2[path[1]]
 
-        # 対応するmcepファイルがなかったら無視する
-        if not os.path.exists(mcep_path2):
-            print('{} not found.'.format(mcep_path2))
-            continue   
-
-        # オリジナルのmcepを読み込み
-        mcep1 = np.loadtxt(mcep_path1)
-        mcep2 = np.loadtxt(mcep_path2)
-
-        # DTWで同期を取る（dtwには引数distが必要）
-        dist, cost, dummy, path = dtw(mcep1, mcep2, dist=euclidean)
-        aligned_mcep1 = mcep1[path[0]]
-        aligned_mcep2 = mcep2[path[1]]
-
-        # 同期を取ったmcepをテキスト形式で書き込み
-        np.savetxt(align_from + file_name, aligned_mcep1, fmt='%0.6f')
-        np.savetxt(align_to + file_name, aligned_mcep2, fmt='%0.6f')
+    # 同期を取ったmcepをテキスト形式で書き込み
+    np.savetxt(out_path_1, aligned_mcep1, fmt='%0.6f')
+    np.savetxt(out_path_2, aligned_mcep2, fmt='%0.6f')
--- main.py
+++ main.py
@@ -1,14 +1,58 @@
 # -*- coding: utf-8 -*-
+import os
 from extract_mcep import extract_mcep
 from align_mcep import align_mcep
 #from train_gmm import train_gmm
 #from convert_voice import convert_voice
 
 
+class VoiceConverter:
+    def __init__(self, conv_from, conv_to):
+        self.__from = conv_from
+        self.__to = conv_to
+
+    def __search_common_wav_files(self):
+        wav_files_from = os.listdir('wav/train/{}/'.format(self.__from))
+        wav_files_to = os.listdir('wav/train/{}/'.format(self.__to))
+        wav_files = list(set(wav_files_from) & set(wav_files_to))
+        # 拡張子無しのファイル名リストの状態で保持しておく
+        self.__train_files = [file.replace('.wav', '') for file in wav_files]
+
+    def __extract_mcep(self):
+        if not os.path.exists('mcep/{}/'.format(self.__from)):
+            os.makedirs('mcep/{}/'.format(self.__from))
+        if not os.path.exists('mcep/{}/'.format(self.__to)):
+            os.makedirs('mcep/{}/'.format(self.__to))
+
+        for file in self.__train_files:
+            wave_path = 'wav/train/{}/{}.wav'.format(self.__from, file)
+            mcep_path = 'mcep/{}/{}.mcep'.format(self.__from, file)
+            extract_mcep(wave_path, mcep_path)
+            wave_path = 'wav/train/{}/{}.wav'.format(self.__to, file)
+            mcep_path = 'mcep/{}/{}.mcep'.format(self.__to, file)
+            extract_mcep(wave_path, mcep_path)
+
+    def __align_mcep(self):
+        outdir = 'mcep_aligned/{}_to_{}/'.format(self.__from, self.__to)
+        if not os.path.exists(outdir + '{}/'.format(self.__from)):
+            os.makedirs(outdir + '{}/'.format(self.__from))
+        if not os.path.exists(outdir + '{}/'.format(self.__to)):
+            os.makedirs(outdir + '{}/'.format(self.__to))
+
+        for file in self.__train_files:
+            mcep_from = 'mcep/{}/{}.mcep'.format(self.__from, file)
+            mcep_to = 'mcep/{}/{}.mcep'.format(self.__to, file)
+            align_mcep_from = outdir + '{}/{}.mcep'.format(self.__from, file)
+            align_mcep_to = outdir + '{}/{}.mcep'.format(self.__to, file)
+            align_mcep(mcep_from, mcep_to, align_mcep_from, align_mcep_to)
+
+    def run(self):
+        self.__search_common_wav_files()
+        self.__extract_mcep()
+        self.__align_mcep()
+
+
+
 if __name__ == '__main__':
-    conv_from = ''
-    conv_to = ''
-    extract_mcep(conv_from, conv_to)
-    align_mcep(conv_from, conv_to)
-    #train_gmm(conv_from, conv_to)
-    #convert_voice(conv_from, conv_to)
+    voice_converter = VoiceConverter('', '')
+    voice_converter.run()
