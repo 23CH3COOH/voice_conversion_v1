c6175e5a771ab440c609f1b8b6254241b4dacf9f
23CH3COOH <kurimannjuu2_ybb_ne_jp@yahoo.co.jp>
2022-05-10 00:22:42 +0900

    元のwavファイルからpitchとmcepを抽出しwavを再合成するテストを作成（しかし最終出力が無音になる模様）

---
+++ audio_file_analyzer.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import csv
+import wave
+from common import run_command
+
+
+def extract_amplitudes(raw_path, out_file_path):
+    res = run_command('dmp +s %s > %s' % (raw_path, out_file_path))
+
+def get_wav_header(wave_path, out_file_path=None, return_all=False):
+    wf = wave.open(wave_path, 'r')
+    channels = wf.getnchannels()
+    frame_rate = wf.getframerate()
+    frame_su = wf.getnframes()
+    time_length = float(wf.getnframes()) / wf.getframerate()
+    sample_byte = wf.getsampwidth()
+    wf.close()
+
+    if out_file_path and type(out_file_path) == str:
+        body = [['チャンネル数', 'Channels', channels],
+                ['サンプリング周波数', 'FrameRate', frame_rate],
+                ['フレーム数', 'Frames', frame_su],
+                ['長さ', 'TimeLength', time_length],
+                ['1サンプルのバイト数', 'SampleWidth', sample_byte]]
+        f = open(out_file_path, 'w', newline='')
+        writer = csv.writer(f)
+        writer.writerows(body)
+        f.close()
+
+    if return_all:
+        return channels, frame_rate, frame_su, time_length, sample_byte
+    else:
+        return channels, frame_rate
---
+++ resynthesis_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from glob import glob
+from audio_file_analyzer import get_wav_header
+from extract_mcep import wav_to_mcep
+from extract_pitch import wav_to_pitch
+from synthesizer import synthesize_to_wav
+
+
+def resynthesize(wav_path):
+    _header = wav_path.replace('.wav', '.header')
+    channels, frame_rate = get_wav_header(wav_path, out_file_path=_header)
+
+    _pitch = wav_path.replace('.wav', '.pitch')
+    wav_to_pitch(wav_path, _pitch)
+
+    _mcep = wav_path.replace('.wav', '.mcep')
+    wav_to_mcep(wav_path, _mcep)
+
+    _out_wav = wav_path.replace('.wav', '_resynthesized.wav')
+    synthesize_to_wav(_pitch, _mcep, _out_wav, channels, frame_rate)
+
+
+if __name__ == '__main__':
+    audio_folder = 'resynthesis_test/'
+    for wav_path in glob(audio_folder + '*.wav'):
+        resynthesize(wav_path)
