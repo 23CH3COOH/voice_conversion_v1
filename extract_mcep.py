9997c600331eee62a9082df655807a3fc4429c8b
23CH3COOH <kurimannjuu2_ybb_ne_jp@yahoo.co.jp>
2022-05-11 00:22:00 +0900

    mcep抽出の際に-eオプションをつけるのをデフォルトとした

--- extract_mcep.py
+++ extract_mcep.py
@@ -16,25 +16,26 @@ def raw_to_mcep(raw_path, mcep_path, binary=False):
         res = run_command([x2x_in, frame, window, mcep, x2x_out])
 
 '''wavファイル(wave_path)からメルケプストラムを抽出しmcep_pathに出力する'''
-def wav_to_mcep(wave_path, mcep_path, binary=False):
+def wav_to_mcep(wave_path, mcep_path, binary=False, add_periodogram=True):
     bcut = 'bcut +s -s 22 %s' % wave_path
     x2x_in = 'x2x +sf'
     frame = 'frame -l 400 -p 80'
     window = 'window -l 400 -L 512'
     if binary:
-        mcep = 'mcep -l 512 -m %d -a %f > %s' % (m, a, mcep_path)
-        res = run_command([bcut, x2x_in, frame, window, mcep], [1])
-        if res == 1:  # 注釈(*)参照
+        if add_periodogram:
             mcep = 'mcep -l 512 -m %d -a %f -e 0.01 > %s' % (m, a, mcep_path)
-            res = run_command([bcut, x2x_in, frame, window, mcep])
+        else:
+            mcep = 'mcep -l 512 -m %d -a %f > %s' % (m, a, mcep_path)
+        res = run_command([bcut, x2x_in, frame, window, mcep])
     else:
-        mcep = 'mcep -l 512 -m %d -a %f' % (m, a)
-        x2x_out = 'x2x +fa%d > %s' % (m + 1, mcep_path)
-        res = run_command([bcut, x2x_in, frame, window, mcep, x2x_out], [1])
-        if res == 1:  # 注釈(*)参照
+        if add_periodogram:  # 注釈(*)
             mcep = 'mcep -l 512 -m %d -a %f -e 0.01' % (m, a)
-            res = run_command([bcut, x2x_in, frame, window, mcep, x2x_out])
+        else:
+            mcep = 'mcep -l 512 -m %d -a %f' % (m, a)
+        x2x_out = 'x2x +fa%d > %s' % (m + 1, mcep_path)
+        res = run_command([bcut, x2x_in, frame, window, mcep, x2x_out])
 
 # (*) 音声によっては以下の警告が出ることがある
 # 'mcep : periodogram has '0', use '-e' option to floor it!'
 # その場合は -e オプションでピリオドグラムに微小な値を足す必要があるらしい
+# 当面扱う音声ではほとんどこの警告が出るので、足すのをデフォルトとした
